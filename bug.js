40682df2e6d4586cd364e7a809431c0acc5d2877
BugHunter-X <askzenai@gmail.com>
2024-12-09 04:58:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,20 @@
+```javascript
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    // This effect runs only once on mount
+    console.log('Mounted!');
+    // Incorrect return statement which will cause a memory leak
+    return () => {
+    };
+  }, []);
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+}
+```
